3c4413ceafa253cefa08ffb9006eb5ed59fd8703
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-18 10:50:00 +0530

    Update and rename RiZoeLXSpam/plugins/alive.py to MAMBAXSPAM/plugins/alive.py

--- MAMBAXSPAM/plugins/alive.py
+++ MAMBAXSPAM/plugins/alive.py
@@ -16,7 +16,7 @@ rizoel += f"┣➣ **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `3.9.6`\n"
 
 rizoel += f"┣➣ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ** : `{version.__version__}`\n"
 
-rizoel += f"┣➣ **ʀɪᴢᴏᴇʟXsᴘᴀᴍ ᴠᴇʀsɪᴏɴ**  : `{mambaversion}`\n"
+rizoel += f"┣➣ **sᴘᴀᴍ ᴠᴇʀsɪᴏɴ**  : `{mambaversion}`\n"
     
 rizoel += f"┣➣ **sᴜᴘᴘᴏʀᴛ** : [JOIN](https://t.me/MAMBA_X_SUPPORT)\n"
 
