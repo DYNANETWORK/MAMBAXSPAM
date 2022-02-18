81406d6f9d40223cbb807ccd44b4ae85ee061d1f
SUKHPAL <86770159+SUKHPAL443@users.noreply.github.com>
2022-02-18 12:04:32 +0530

    Update __init__.py

--- MAMBAXSPAM/__init__.py
+++ MAMBAXSPAM/__init__.py
@@ -20,7 +20,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 #version
 
-mambaversion = "v2.0.0"
+
 
 #values
 API_ID = config("API_ID", default=None, cast=int)
